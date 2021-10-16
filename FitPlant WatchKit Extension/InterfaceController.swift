c6220cbaa35249f3d1c1a794145311cc1149ecfe
Vincent <vbn3@illinois.edu>
2021-10-15 20:10:21 -0500

    Initial Commit

---
+++ FitPlant WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  FitPlant WatchKit Extension
+//
+//  Created by Vincent Nguyen on 10/15/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ FitPlant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitPlant
+//
+//  Created by Vincent Nguyen on 10/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
